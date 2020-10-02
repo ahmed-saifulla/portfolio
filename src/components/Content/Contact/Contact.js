4672e5358f89b5bde99d30003467603d595cb8dc
Ahmed Saifulla N S <ahsaifulla@gmail.com>
2020-10-02 14:33:35 +0530

    social links

--- src/components/Content/Contact/Contact.js
+++ src/components/Content/Contact/Contact.js
@@ -35,24 +35,24 @@ function Contact() {
         <div className="right">
           <h3>Stay in touch</h3>
           <div className="medias">
-            <a href="#!" className="media">
+            <a href="https://github.com/ahmed-saifulla" target="_blank" className="media">
               <img src={github} alt="" />
               <h6>Github</h6>
             </a>
-            <a href="#!" className="media">
+            <a href="mailto:ahsaifulla@gmail.com" target="_blank" className="media">
               <img src={gmail} alt="" />
               <h6>Gmail</h6>
             </a>
-            <a href="#!" className="media">
+            <a href="https://www.linkedin.com/in/ahmed-saifulla/" target="_blank" className="media">
               <img src={linkedin} alt="" />
               <h6>LinkedIn</h6>
             </a>
             <div className="break"></div> {/*--------------*/}
-            <a href="#!" className="media">
+            <a href="https://www.instagram.com/ah_saifullah/" target="_blank" className="media">
               <img src={instagram} alt="" />
               <h6>Instagram</h6>
             </a>
-            <a href="#!" className="media">
+            <a href="https://wa.me/918129318510" target="_blank" className="media">
               <img src={whatsapp} alt="" />
               <h6>Whatsapp</h6>
             </a>
